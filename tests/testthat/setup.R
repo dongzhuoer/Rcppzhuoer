878bd44dc9e2afd6043142a87b3ee7294ff16e8c
Zhuoer Dong <dongzhuoer@mail.nankai.edu.cn>
2019-01-12 12:19:48 +0800

    fix test C++ export

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,7 +1,9 @@
 testthat::context('Initialize environment')
+if (basename(getwd()) == 'testthat') setwd('../..')  # workspace is reset per file
 # refer to http://testthat.r-lib.org/reference/test_dir.html#test-files
 
 options(testthat_quick = F);
 
-
+# this package is kinds of special, it has to be installed for testing C++ export
+if (!'Rcppzhuoer' %in% .packages(T)) callr::rcmd('INSTALL', getwd())
 
